42da4e2cb9e1f55db24a5095706247df83a124c9
Emil Jan <anoldwebsite@gmail.com>
2021-06-13 08:33:38 +0100

    landing page completed with Grid system

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from '@material-ui/styles'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/ui/header';
+import Footer from '../components/ui/footer';
+import theme from './ui/theme';
+import LandingPage from '../components/landingPage';
+
+function App() {
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+          />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contact Us</div>} />
+          <Route exact path="/estimate" component={() => <div>Get a Free Estimate</div>} />
+        </Switch>
+        <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
